55161be0c87b115cf4dd8bdb88ef86545ed52cd5
Nalinda Gamaarachchi <44144772+NalindaGamaarachchi@users.noreply.github.com>
2023-05-11 20:22:26 +0530

    Updates

---
+++ precache-manifest.4e640b4fb7f4a6e4a4159ce03d131b91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b797f66bf1cdd7d30834c6a98a421424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a56c45e257b91ae903b0",
+    "url": "/static/css/main.db03f86e.chunk.css"
+  },
+  {
+    "revision": "33520d66f5b3060eb56a",
+    "url": "/static/js/2.0d9a8510.chunk.js"
+  },
+  {
+    "revision": "a56c45e257b91ae903b0",
+    "url": "/static/js/main.c9dd9406.chunk.js"
+  },
+  {
+    "revision": "80b789232f75104194ee",
+    "url": "/static/js/runtime-main.e2fb13b6.js"
+  }
+]);
