1617ab2179a07fa792b116af456b37aa9cfe3778
Nalinda Gamaarachchi <44144772+NalindaGamaarachchi@users.noreply.github.com>
2023-05-11 19:52:15 +0530

    add files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,121 @@
-import { hydrate, render } from 'react-dom'
-import App from './app'
+import ReactDOM from "react-dom"
+import React, { Suspense, useEffect, useRef, useMemo } from "react"
+import { Canvas, Dom, useLoader, useFrame } from "react-three-fiber"
+import { TextureLoader, LinearFilter } from "three"
+import lerp from "lerp"
+import { Text, MultilineText } from "./components/Text"
+import Diamonds from "./diamonds/Diamonds"
+import Plane from "./components/Plane"
+import { Block, useBlock } from "./blocks"
+import state from "./store"
+import "./styles.css"
 
-const rootElement = document.getElementById('root')
-
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement)
-} else {
-    render(<App />, rootElement)
+function Startup() {
+  const ref = useRef()
+  useFrame(() => (ref.current.material.opacity = lerp(ref.current.material.opacity, 0, 0.025)))
+  return <Plane ref={ref} color="#0e0e0f" position={[0, 0, 200]} scale={[100, 100, 1]} />
 }
+
+function Paragraph({ image, index, offset, factor, header, aspect, text }) {
+  const { contentMaxWidth: w, canvasWidth, margin, mobile } = useBlock()
+  const size = aspect < 1 && !mobile ? 0.65 : 1
+  const alignRight = (canvasWidth - w * size - margin) / 2
+  const pixelWidth = w * state.zoom * size
+  const left = !(index % 2)
+  const color = index % 2 ? "#D40749" : "#2FE8C3"
+  return (
+    <Block factor={factor} offset={offset}>
+      <group position={[left ? -alignRight : alignRight, 0, 0]}>
+        <Plane map={image} args={[1, 1, 32, 32]} shift={75} size={size} aspect={aspect} scale={[w * size, (w * size) / aspect, 1]} frustumCulled={false} />
+
+        <Dom
+          style={{ width: pixelWidth / (mobile ? 1 : 1.5), textAlign: 0 ? "right" : "left" }}
+          position={[left || mobile ? (w * size) / 2 : (-w * size) / 2, mobile ? 0: (w * size) / 2 / aspect - 0.1, -10]}>
+          {text}
+        </Dom>
+
+        <Dom style={{ width: pixelWidth / (mobile ? 1 : 2), textAlign: 0 ? "left" : "right" }} position={[left || mobile ? (-w * size) / 2 : 0, (-w * size) / 2 / aspect - 0.4, 1]}>
+          {/* <div tabIndex={index}>{text}</div> */}
+        </Dom>
+        <Text left={left} right={!left} size={w * 0.04} color={color} top position={[((left ? -w : w) * size) / 2, (w * size) / aspect / 2 + 0.5, -1]}>
+          {header}
+        </Text>
+      </group>
+    </Block>
+  )
+}
+
+function Content() {
+  const images = useLoader(
+    TextureLoader,
+    state.paragraphs.map(({ image }) => image)
+  )
+  useMemo(() => images.forEach((texture) => (texture.minFilter = LinearFilter)), [images])
+  const { contentMaxWidth: w, canvasWidth, canvasHeight, mobile } = useBlock()
+  return (
+    <>
+      <Block factor={1} offset={0}>
+        <Block factor={1.2}>
+          <Text left size={w * 0.08} position={[-w / 2, 0.5, -1]} color="#d40749">
+            Nalinda
+          </Text>
+          <Text left size={w * 0.08} position={[-w / 2, -1.0, -1]} color="#d40749">
+            Gamaarachchi
+          </Text>
+        </Block>
+        <Block factor={1.0}>
+          <Dom position={[-w / 2, -w * 0.08 + 2.3, -1]}>Hello I'm,</Dom>
+        </Block>
+      </Block>
+      <Block factor={1.2} offset={5.7}>
+        <MultilineText top left size={w * 0.15} lineHeight={w / 5} position={[-w / 3.5, 0, -1]} color="#2fe8c3" text={"four\nzero\nzero"} />
+      </Block>
+      {state.paragraphs.map((props, index) => (
+        <Paragraph key={index} index={index} {...props} image={images[index]} />
+      ))}
+      {state.stripes.map(({ offset, color, height }, index) => (
+        <Block key={index} factor={-1.5} offset={offset}>
+          <Plane args={[50, height, 32, 32]} shift={-4} color={color} rotation={[0, 0, Math.PI / 8]} position={[0, 0, -10]} />
+        </Block>
+      ))}
+      <Block factor={1.25} offset={8}>
+        <Dom className="bottom-left" position={[-canvasWidth / 2, -canvasHeight / 2, 0]}>
+          Culture is not your friend.
+        </Dom>
+      </Block>
+    </>
+  )
+}
+
+function App() {
+  const scrollArea = useRef()
+  const onScroll = (e) => (state.top.current = e.target.scrollTop)
+  useEffect(() => void onScroll({ target: scrollArea.current }), [])
+  return (
+    <>
+      <Canvas className="canvas" concurrent pixelRatio={1} orthographic camera={{ zoom: state.zoom, position: [0, 0, 500] }}>
+        <Suspense fallback={<Dom center className="loading" children="Loading..." />}>
+          <Content />
+          <Diamonds />
+          <Startup />
+        </Suspense>
+      </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        {new Array(state.sections).fill().map((_, index) => (
+          <div key={index} id={"0" + index} style={{ height: `${(state.pages / state.sections) * 100}vh` }} />
+        ))}
+      </div>
+      <div className="frame">
+        <div className="frame__nav">
+          <a className="frame__link" href="#00" children="home" />
+          <a className="frame__link" href="#01" children="about" />
+          <a className="frame__link" href="#02" children="projects" />
+          <a className="frame__link" href="#03" children="winning" />
+          <a className="frame__link" href="#04" children="contact" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
